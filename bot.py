59e47c0847f8f1c84b8ff4890bb730242d7786e7
jamolanguekd <jamolanguekd@gmail.com>
2021-04-28 03:02:48 +0800

    fixed token access

--- bot.py
+++ bot.py
@@ -1,9 +1,6 @@
 import os
-
 import discord
-from dotenv import load_dotenv
 
-load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
 client = discord.Client()
@@ -17,6 +14,8 @@ async def on_message(message):
     if message.author == client.user:
         return
     
+    # TODO: Encapsulate
+    # MURA RESPONDER
     curseVocab = ["TANGINA","GAGO","PAKYU"]
     response = ""
     for word in curseVocab:
