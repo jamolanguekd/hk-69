fb60946c9b4d6809edb6ff3fd94a7053c12a6ed0
jamolanguekd <jamolanguekd@gmail.com>
2021-10-20 02:45:07 +0800

    restructure features into individual folders

--- bot.py
+++ bot.py
@@ -7,5 +7,5 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 bot = commands.Bot("")
 cogs = ["maincog","polls","responses","music"]
 for cog in cogs:
-    bot.load_extension(f"cogs.{cog}")
+    bot.load_extension(f"cogs.{cog}.{cog}")
 bot.run(TOKEN)
