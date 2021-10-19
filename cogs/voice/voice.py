83a63b59127491a054dba37cff0948228a142b88
jamolanguekd <jamolanguekd@gmail.com>
2021-10-20 07:11:42 +0800

    add basic pause function

--- cogs/voice/voice.py
+++ cogs/voice/voice.py
@@ -18,17 +18,35 @@ class Voice(commands.Cog):
     
     @music.command()
     async def play(self, ctx, *, arg):
-        loop = self.bot.loop or asyncio.get_running_loop()
-        stream_data = await loop.run_in_executor(None, lambda: youtube_helper.get_stream_data(arg))
-        stream_url = stream_data['url']
-        stream_title = stream_data['title']
-        stream_duration = seconds_to_dhm(stream_data['duration'])
-
+        # Voice client should always be present!
         voice_client = ctx.voice_client
-        voice_client.play(discord.FFmpegPCMAudio(stream_url, **youtube_helper.FFMPEG_OPTIONS), after = lambda e : None)
 
-        msg = self.create_playing_embed(ctx, stream_title, stream_duration)
-        await ctx.send(embed = msg)
+        if voice_client.is_playing():
+            # Check channel
+                # If command channel is different from vc channel, display error
+                # Else, add to queue
+            # Add to queue
+            None
+        
+        elif voice_client.is_paused():
+            if not arg:
+                voice_client.resume()
+            #If arg exists
+                # add to queue
+                # play current song
+        
+        else:
+            loop = self.bot.loop or asyncio.get_running_loop()
+            stream_data = await loop.run_in_executor(None, lambda: youtube_helper.get_stream_data(arg))
+            stream_url = stream_data['url']
+            stream_title = stream_data['title']
+            stream_duration = seconds_to_dhm(stream_data['duration'])
+
+            
+            voice_client.play(discord.FFmpegPCMAudio(stream_url, **youtube_helper.FFMPEG_OPTIONS), after = lambda e : None)
+
+            msg = self.create_playing_embed(ctx, stream_title, stream_duration)
+            await ctx.send(embed = msg)
 
     def create_playing_embed(self, ctx, title, duration):
         msg = discord.Embed()
@@ -38,6 +56,12 @@ class Voice(commands.Cog):
         msg.timestamp = ctx.message.created_at
         return msg 
 
+    @music.command()
+    async def pause(self, ctx):
+        voice_client = ctx.voice_client
+        if(voice_client and voice_client.is_playing()):
+            voice_client.pause()
+    
     @music.command()
     async def stop(self, ctx):
         voice_client = ctx.voice_client
